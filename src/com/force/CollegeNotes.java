649bdb1944d39bbb9d4e7877f50a5dd4afc41be4
Aerogit <siva319mca@gmail.com>
2022-04-19 22:32:51 +0530

    housekeeper work as done

--- src/com/force/CollegeNotes.java
+++ src/com/force/CollegeNotes.java
@@ -1,7 +1,9 @@
 package com.force;
 
 public class CollegeNotes {
-	
+	private void attendance() {
+		System.out.println("attendance");
+	}
 	private void db() {
 		System.out.println("database");
 
